fed7253990647f741e29f5ba19a4c57d632677c8
sangameswahr <112796162+sangameswahr@users.noreply.github.com>
2022-09-24 14:32:38 +0530

    Adding HelloWin

---
+++ HelloWin.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+#pragma comment(lib, "kernel32.lib");
+#pragma comment(lib, "user32.lib");
+#pragma comment(lib, "gdi32.lib");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPCSTR lpCmdLine, int nShowCmd)
+{
+    static TCHAR szClassName = TEXT("My first app");
+    static TCHAR szWindowName = TEXT("We Are Rocking Win32");
+
+    WNDCLASSEX wndEx;
+
+    HBRUSH hbrBackGround;
+    HICON hIcon;
+    HICON hIconSm;
+    HCURSOR hCursor;
+
+    hbrBackGround = 
+
+}
