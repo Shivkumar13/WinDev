2633d52f187ab1992cb8c5f6cd976bb99f10aac7
Shivkumar Anil Ople <shivkumar.ople97@gmail.com>
2022-12-15 20:25:36 +0530

    Adding Snake Design

---
+++ snake_design.cpp
@@ -0,0 +1,275 @@
+// SnakeApp.cpp : Defines the entry point for the application.
+//
+
+#include <windows.h>
+#include "framework.h"
+#include "SnakeApp.h"
+#include <objidl.h>
+#include <gdiplus.h>
+#include <gdiplusgraphics.h>
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+static RECT rcTarget;
+
+
+VOID OnPaint(HDC hdc)
+{
+    Graphics graphics(hdc);
+
+    //Snake Body
+    Point points[] = { Point(100, 57),
+                  Point(200, 100),
+                  Point(300, 55),
+                  Point(400, 120),
+                  Point(500, 85),
+                  Point(580, 150)
+    };
+
+    Point points1[] = { Point(105, 35),
+                 Point(210, 90),
+                 Point(310, 45),
+                 Point(410, 105),
+                 Point(510, 75), 
+                 Point(580, 150)
+                 };
+
+    //Snake Eye
+    Point points2[] = { Point(100, 60),
+               Point(100, 60),
+               Point(100, 60),
+               Point(100, 60),
+               Point(100, 60)
+    };
+
+    Pen      pen(Color(255, 0, 0, 255));
+    //graphics.DrawCurve(&pen, points, 5, 0.0f); 
+    //graphics.DrawCurve(&pen, points, 5, 0.06);
+    //Snake Body
+    graphics.DrawCurve(&pen, points, 6, 0.7f);
+    graphics.DrawCurve(&pen, points1, 6, 0.8f);
+
+    //Snake Eye
+    graphics.DrawLine(&pen, 100, 40, 101, 44);
+
+    graphics.DrawLine(&pen, 70, 50, 50, 55);
+    graphics.DrawLine(&pen, 70, 50, 50, 40);
+
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+    //graphics.DrawLine(&pen, 100, 39, 101, 44);
+
+
+    //graphics.DrawLine(&pen, points2, 6, 0.5f);
+}
+
+VOID DrawArc(HDC hdc)
+{
+    Graphics graphics(hdc);
+
+    // Set up the arc.
+    Pen pen(Color(255, 0, 0, 255));
+    REAL x = 65;
+    REAL y = 37;
+    REAL width = 80.0f;
+    REAL height = 20.0f;
+    REAL startAngle = 115.0f;
+    REAL sweepAngle = 190.0f;
+
+    // Draw the arc.
+    graphics.DrawArc(&pen, x, y, width, height, startAngle, sweepAngle);
+}
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR           gdiplusToken;
+    MSG                 msg;
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_SNAKEAPP, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_SNAKEAPP));
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SNAKEAPP));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_SNAKEAPP);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            OnPaint(hdc);
+            DrawArc(hdc);
+            Ellipse(ps.hdc, rcTarget.left, rcTarget.top,
+                rcTarget.right, rcTarget.bottom);
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
